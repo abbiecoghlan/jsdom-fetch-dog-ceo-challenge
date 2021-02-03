fed5a41e790932c08d59dc9d2906e518d28bf179
“Abbie <abbie.coghlan@gmail.com>
2021-02-03 11:21:49 -0600

    commit tree f91efa6f4bae2f053ba0017c2cfe7f2c60db4ced

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+
+function fetchImageInfo(){
+    fetch(imgUrl)
+    .then( res => res.json() )
+    .then( imageInfo => {
+        addImagesToDOM(imageInfo)
+    })
+}
+
+function addImagesToDOM(imageInfo){
+    let dogContainer = document.querySelector("#dog-image-container")
+    let allTheDogs = imageInfo.message.map(function(dog){
+        let newImage = document.createElement('img') 
+        newImage.setAttribute("src", dog)
+        dogContainer.append(newImage)
+    })
+}
+
+fetchImageInfo()
